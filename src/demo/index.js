8171014c3526425afb402348821e94d0b430dbae
ivan minutillo <minutillo.ivan@gmail.com>
2018-04-09 13:44:38 +0200

    demo enviroment updated

--- src/demo/index.js
+++ src/demo/index.js
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Login from './login/'
 import Settings from './settings/wrapper'
+import Agent from './agent/wrapper'
+import Overview from './overview/wrapper'
 import registerServiceWorker from './registerServiceWorker'
-import {HashRouter as Router, Route} from 'react-router-dom'
+import {HashRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 import {ApolloProvider} from 'react-apollo'
 import {client, store} from './store'
 import { Provider } from 'react-redux'
@@ -11,6 +16,19 @@ import PrivateRoute from './templates/AppTemplate'
 import { Notifs } from 'redux-notifications'
 import style from './base.css'
 import Validate from '../lib/validate'
+// import Validate from 'oce-validate'
+// import workStyle from "collaborate/build/css/index.css"
+// import validateStyle from "oce-validate/build/css/index.css"
+
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>
+      No match for <code>{location.pathname}</code>
+    </h3>
+  </div>
+);
+
+
 function CustomNotif (props) {
   let type
   if (props.kind === 'danger') { type = style.danger } else
@@ -28,18 +46,22 @@ ReactDOM.render(
   <ApolloProvider client={client}>
     <Provider store={store}>
       <Router>
-      <div>
-        <Notifs
-          CustomComponent={CustomNotif}
-          onActionClick={id => this.dismiss(id)}
-          actionLabel='close'
-        />
-        <Route exact path='/login' component={Login} />
-        <PrivateRoute exact path='/' component={() => (<h1>overview</h1>)} />
-        <PrivateRoute path='/work' component={() => (<h1>overview</h1>)} />
-        <PrivateRoute path='/validate' component={Validate} />
-        <PrivateRoute path='/settings' component={Settings} />
-      </div>
+        <div>
+          <Notifs
+            CustomComponent={CustomNotif}
+            onActionClick={id => this.dismiss(id)}
+            actionLabel='close'
+          />
+          <Switch>
+            <Route path='/login' component={Login} />
+            <PrivateRoute exact path='/' component={Overview} />
+            <PrivateRoute exact path='/agent/:id' component={Agent} />
+            {/* <PrivateRoute path='/work' component={Work} /> */}
+            <PrivateRoute path='/validate' component={Validate} />
+            <PrivateRoute path='/settings' component={Settings} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
       </Router>
     </Provider>
   </ApolloProvider>,
